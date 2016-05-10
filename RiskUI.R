90f49fc8a626d713914888113473fd7f796a9bd6
sor16 <solviro@gmail.com>
2016-05-10 12:39:51 +0000

    fixing reactiveness

--- RiskUI.R
+++ RiskUI.R
@@ -4,34 +4,32 @@ library(dplyr)
 ui <- fluidPage(
     column(width=4,
         titlePanel("Áhættureiknir"),
-        selectInput(inputId="Age",label="Select yout age",choices=as.character(1:100)),
-        actionButton(inputId="backward","Previous Plot"),
-        actionButton(inputId="forward","Next Plot")
+        selectInput(inputId="Age",label="Select yout age",choices=as.character(1:100))
     ),
     column(width=8,
-        plotOutput("plot")
+        plotOutput("plot"),
+        actionButton(inputId="backward","Previous Plot"),
+        actionButton(inputId="forward","Next Plot")
     )
 )
 server <- function(input,output){
-    plots=reactiveValues(i=1,max=10)
+    plots=reactiveValues(i=1,max=10,List=list())
     plotting <- reactive({
         samples=replicate(10,sample_n(diamonds,100))
-        plotList=lapply(1:10,function(i) ggplot(data=as.data.frame(samples[,i]),aes(carat,price)) +
+        plots$List=lapply(1:10,function(i) ggplot(data=as.data.frame(samples[,i]),aes(carat,price)) +
                             geom_point() +theme_bw() + labs(title=(paste("Plot nr.",i))))
-    })
-    plotNr <- reactive({
-        plotList[[plots$i]]
+        plots$List[[plots$i]]
     })
     output$plot <- renderPlot({
-        plotNr()
+        plotting()
     })
     observeEvent(input$forward,{
         plots$i=ifelse(plots$i==10,plots$i%%plots$max+1,plots$i+1)
-        plotNr()
+        plotting()
     })
     observeEvent(input$backward,{
         plots$i=ifelse(plots$i==1,plots$i+plots$max-1,plots$i-1)
-        plotNr()
+        plotting()
     })
     
 }
